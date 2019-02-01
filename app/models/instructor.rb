2d8894da6be6dd589a16438f686a547d5e2d13d6
github username <github email address>
2019-01-31 20:04:14 -0800

    Fixed pry failure by commenting out pry in console.rb

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -11,13 +11,13 @@ class Instructor
   end
 
   def pass_student(student, name)
-    t = (BoatingTest.all.select { |b| b.student == student && b.name == name }
+    t = BoatingTest.all.select { |b| b.student == student && b.name == name }
     !!(t) ? t.status = "passed" : t = BoatingTest.new(student, name, "passed", self)
     t
   end
 
   def fail_student(student, name)
-    t = (BoatingStudent.all.select { |b| b.student == student && b.name == name }
+    t = BoatingStudent.all.select { |b| b.student == student && b.name == name }
     !!(t) ? t.status = "failed" : t = BoatingTest.new(student, name, "failed", self)
     t
   end
