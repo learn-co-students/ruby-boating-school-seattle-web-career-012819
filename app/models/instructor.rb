8f0fcf459cfd0d6f29d6d018f86209dad889b3c1
tiefengeist <sahirnambiar@gmail.com>
2019-01-31 21:37:05 -0800

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,37 @@
 class Instructor
+  attr_accessor :name
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(first_name, testname)
+    test = first_name.tests.select do |test|
+      test.testname == testname
+    end
+    if test.size == 0
+      BoatingTest.new(first_name, testname, 'passed', self)
+    else
+      test[0].status = 'passed'
+    end
+  end
+
+  def fail_student(first_name, testname)
+    test = first_name.tests.select do |test|
+      test.testname == testname
+    end
+    if test.size == 0
+      BoatingTest.new(first_name, testname, 'failed', self)
+    else
+      test[0].status = 'failed'
+    end
+  end
+  
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,40 @@
 class Student
 
+  attr_accessor :first_name
+
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def add_boating_test(testname, status, instructor)
+    BoatingTest.new(self, testname, status, instructor)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_student(first_name)
+    self.all.select do |student|
+      student.first_name == first_name
+    end
+  end
+
+  def tests
+    BoatingTest.all.select do |test|
+      test.first_name == self
+    end
+  end
+
+  def grade_percentage
+    total_tests = self.tests
+    tests_passed = total_tests.reduce(0) do |pass, test|
+      pass += test.status == 'passed' ? 1 : 0
+    end
+    tests_passed * 100 / (total_tests.size * 1.0)
+  end
+
 end
