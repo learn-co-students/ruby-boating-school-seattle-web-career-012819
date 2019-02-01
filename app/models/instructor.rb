5fe9f0e95b65ed9f9b7e8eb786992cf686b0f959
github username <github email address>
2019-01-31 17:51:06 -0800

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,39 @@
 class Instructor
 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def pass_student(student, test_name)
+    test = student.tests.select do |test|
+      test.test_name == test_name
+    end
+    if test.empty?
+      BoatingTest.new(student, test_name, "passed", self)
+    else
+      test[0].status = "passed"
+    end
+  end
+
+  def fail_student(student, test_name)
+    test = student.tests.select do |test|
+      test.test_name == test_name
+    end
+    if test.empty?
+      BoatingTest.new(student, test_name, "failed", self)
+    else
+      test[0].status = "failed"
+    end
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,41 @@
 class Student
 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(test_name, status, instructor)
+    BoatingTest.new(self, test_name, status, instructor)
+  end
+
+  def self.find_student(name)
+    self.all.select do |student|
+      student.name == name
+    end
+  end
+
+  def tests
+    BoatingTest.all.select do |test|
+      test.student == self
+    end
+  end
+
+  def grade_percentage
+    students_tests = self.tests
+    total_score = students_tests.reduce(0) do |total, test|
+      total += test.status == "passed" ? 1 : 0
+    end
+    total_score / (students_tests.length * 1.0)
+  end
+
 end
