40f89b4cbf23e79f7c34a07442e8a125cc4c37f7
github username <github email address>
2019-01-31 19:50:07 -0800

    First commit.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,24 @@
 class Instructor
+  @@all = []
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def pass_student(student, name)
+    t = (BoatingTest.all.select { |b| b.student == student && b.name == name }
+    !!(t) ? t.status = "passed" : t = BoatingTest.new(student, name, "passed", self)
+    t
+  end
+
+  def fail_student(student, name)
+    t = (BoatingStudent.all.select { |b| b.student == student && b.name == name }
+    !!(t) ? t.status = "failed" : t = BoatingTest.new(student, name, "failed", self)
+    t
+  end
 end
