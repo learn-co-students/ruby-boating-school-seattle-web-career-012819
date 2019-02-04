8c802d46ebd1e90f5c7d3e767a89dd3b56fe6506
Nat-Reid <nathanielreid@gmail.com>
2019-02-03 21:00:47 -0800

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,27 @@
 class BoatingTest
 
+  attr_reader :student, :test_name, :instructor
+  attr_accessor  :status
+
+  @@all = []
+
+  def initialize(student: nil, test_name: "", status: "pending", instructor: nil)
+    @student = student
+    @test_name = test_name
+    @status = status
+    @instructor = instructor
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_test(student, instructor, test_name)
+    self.all.find{|test| test.student == student
+                        && test.instructor == instructor
+                        && test.test_name == test_name}
+  end
+
 end
