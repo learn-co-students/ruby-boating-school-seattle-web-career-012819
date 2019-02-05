024eb2aaef58befef65be4ce06e2a6e25c9fdd0c
MatthewBrophy <Brophy3030@gmail.com>
2019-02-04 18:43:36 -0800

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,30 @@
 class Instructor
+  attr_reader :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all.append(self)
+  end
+  
+  def self.all
+    @@all
+  end
 
+  def pass_student(student, test_name)
+    find_test = BoatingTest.all.find{|test| test.student.first_name == student.first_name && test.test_name == test_name}
+    if find_test
+      find_test.status = "passed"
+    else
+      BoatingTest.new(student, test_name, "pending", self)
+    end
+  end
+
+  def fail_student(student, test_name)
+    testtofail= BoatingTest.all.find{|test| test.student.first_name == student.first_name && test.name == test_name}
+    if testtofail
+      testtofail.status= "failed"
+    else
+      BoatingTest.new(student, test_name, "failed", self)
+    end
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 class Student
+  attr_reader :first_name
+  @@all = []
+  def initialize(first_name)
+    @first_name = first_name
+    @@all.append(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+def add_boating_test(test_name, status, instructor)
+  BoatingTest.new(self, test_name, status, instructor)
+end
+
+def self.find_student(student_first_name)
+  all.select {|x| x.first_name == student_first_name}
+end
+
+def grade_percentage
+  tests_taken = BoatingTest.all.select {|test| test.student.first_name == self.first_name}.count
+  tests_passed = BoatingTest.all.select {|test| test.student.first_name == self.first_name && test.status == "passed"}.count
+  (tests_passed.to_f/tests_taken.to_f) * 100
+end
 
 end
