48e79c4ede145bc0a0546783accab3dd1242ecb9
Katie Carrel <katiecarrel@comcast.net>
2019-02-04 20:09:07 -0800

    adding files

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,38 @@
 class Instructor
 
+  @@all = []
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(student,test_name)
+    BoatingTest.all.select do |test|
+      if test.student == student && test.boating_test_name == test_name
+        test.boating_test_status = "passed"
+        return test
+      end
+    end
+    BoatingTest.new(student,test_name,"passed",self)
+    return BoatingTest.all.last
+  end
+
+  def fail_student(student,test_name)
+    BoatingTest.all.select do |test|
+      if test.student == student && test.boating_test_name == test_name
+        test.boating_test_status = "failed"
+        return test
+      end
+    end
+    BoatingTest.new(student,test_name,"failed",self)
+    returning BoatingTest.all.last
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,45 @@
 class Student
 
+  attr_accessor :first_name
+
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(boating_test_name,boating_test_status,instructor)
+    BoatingTest.new(self,boating_test_name,boating_test_status,instructor)
+  end
+
+  def self.find_student(first_name)
+    Student.all.select do |student|
+      if student.first_name == first_name
+        return student
+      end
+    end
+  end
+
+  def grade_percentage
+    pass_count = 0
+    fail_count = 0
+    BoatingTest.all.select do |test|
+      if test.student == self
+        if test.boating_test_status == "passed"
+          pass_count += 1
+        elsif test.boating_test_status == "failed"
+          fail_count += 1
+        end
+      end
+    end
+    total = pass_count + fail_count
+    return pass_count.to_f/total
+  end
+
 end
